cf3281c6ef5884b36203cbb0c2aa27b4d1451945
Andreas Haas <andreas.haas1@gmx.de>
2015-05-16 17:06:38 +0200

    linked list for member/formation

--- src/member.c
+++ src/member.c
@@ -6,7 +6,11 @@
 #include "request.h"
 #include "member.h"
 
+#define MAX_STR 100
+
+struct member_struct;
 struct member_struct {
+	struct member_struct *next;
 	char *first;
 	char *second;
 	char *street;
@@ -17,83 +21,113 @@ struct member_struct {
 	int formation;
 };
 
-#define MAX_MEMBER 100
-#define MAX_STR 100
-struct member_struct member[MAX_MEMBER];
-int nmember;
+static struct member_struct *member_list = NULL;
+static int nmember;
 
+struct formation_struct;
 struct formation_struct {
+	struct formation_struct *next;
 	char *name;
-	int super;
+	int fid;
+	struct formation_struct *super;
 };
 
-#define MAX_FORMATION 50
-struct formation_struct formation[MAX_FORMATION];
-int nformation;
+static struct formation_struct *formation_list = NULL;
+static int nformation;
 
 // UTF-8
-int read_member_list(const char *file)
+int read_member_list(const char *path)
 {
-	FILE *fp;
+	FILE *file;
 	char buffer[8][MAX_STR];
 	int i = 0;
+	struct member_struct *m, **mp = &member_list; 
 
-	if (!(fp=fopen(file, "r"))) 
+	if (!(file=fopen(path, "r"))) 
 		return -1;
 
-	while (fscanf(fp, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\n]\n", 
+	while (fscanf(file, "%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\t]\t%[^\n]\n", 
 			buffer[0], buffer[1], buffer[2], buffer[3],
 			buffer[4], buffer[5], buffer[6], buffer[7]) == 8) {
-		if (i == MAX_MEMBER) {
-			fprintf(stderr, "too many members!!\n");
-			return -1;
-		}
-		member[i].first     = strdup(buffer[0]);
-		member[i].second    = strdup(buffer[1]);
-		member[i].street    = strdup(buffer[2]);
-		member[i].house     = strdup(buffer[3]);
-		member[i].zip       = strdup(buffer[4]);
-		member[i].city      = strdup(buffer[5]);
-		member[i].email     = strdup(buffer[6]);
-		if (sscanf(buffer[7], "%d", &member[i].formation)!=1) {
-			fprintf(stderr, "bad formation number!!\n");
-			return -1;
-		}
 
-		/* fprintf(stderr, "read: %s %s\n", member[i].first, member[i].second); */
+		if (!(m = (struct member_struct *)malloc(sizeof(struct member_struct)))) { 
+			fprintf(stderr, "malloc() failure!!\n");
+			fclose(file);
+			return -1;
+		}
+		m->first     = strdup(buffer[0]);
+		m->second    = strdup(buffer[1]);
+		m->street    = strdup(buffer[2]);
+		m->house     = strdup(buffer[3]);
+		m->zip       = strdup(buffer[4]);
+		m->city      = strdup(buffer[5]);
+		m->email     = strdup(buffer[6]);
+		m->next = NULL;
+		if (sscanf(buffer[7], "%d", &m->formation)!=1) {
+			fprintf(stderr, "bad formation number!!\n");
+			fclose(file);
+			return -1;
+		}
+		*mp = m;
+		mp = &m->next;
+
+		/* fprintf(stderr, "read: %s %s\n", m->first, m->second); */
 		i++;
 	}
 		
-	fclose(fp);
+	fclose(file);
 
 	nmember = i;
 	return i;
 }
 
-int read_formation_list(const char *file) 
+static struct formation_struct *get_formation(int fid)
 {
-	FILE *fp;
-	char buffer[3][MAX_STR];
-	int i = 0;
+	struct formation_struct *f = formation_list;
+	for (f = formation_list; f && f->fid != fid; f = f->next)
+		;
+	return f;
+}
 
-	if (!(fp=fopen(file, "r"))) 
+int read_formation_list(const char *path) 
+{
+	FILE *file;
+	char buffer[3][MAX_STR];
+	int super, i = 0;
+	struct formation_struct *f, **fp = &formation_list;
+
+	if (!(file=fopen(path, "r"))) 
 		return -1;
 
-	while (fscanf(fp, "%[^\t]\t%[^\t]\t%[^\n]\n", buffer[0], buffer[1], buffer[2]) == 3) {
-		if (i == MAX_FORMATION) {
-			fprintf(stderr, "too many formations!!\n");
+	while (fscanf(file, "%[^\t]\t%[^\t]\t%[^\n]\n", buffer[0], buffer[1], buffer[2]) == 3) {
+		if (!(f = (struct formation_struct *)malloc(sizeof(struct formation_struct)))) { 
+			fprintf(stderr, "malloc() failure!!\n");
+			fclose(file);
 			return -1;
 		}
-		formation[i].name      = strdup(buffer[1]);
-		if (sscanf(buffer[2], "%d", &formation[i].super)!=1) {
+		f->name = strdup(buffer[1]);
+		f->fid = i;
+		f->next = NULL;
+
+		if (sscanf(buffer[2], "%d", &super)!=1) {
 			fprintf(stderr, "bad super formation!!\n");
+			fclose(file);
 			return -1;
 		}
-		/* fprintf(stderr, "read: %s %d\n", formation[i].name, formation[i].super); */
+
+		if (!(f->super = get_formation(super)) && i != 0) {
+			fprintf(stderr, "no formation with fid %d!!\n", super);
+			fclose(file);
+			return -1;
+		}
+
+		*fp = f;
+		fp = &f->next;
+		/* fprintf(stderr, "read: %s %d\n", f->name, f->fid); */
 		i++;
 	}
 		
-	fclose(fp);
+	fclose(file);
 
 	nformation = i;
 	return i;
@@ -115,26 +149,23 @@ int member_iterator(void *cls, enum MHD_ValueKind kind, const char *key,
 
 static int in_formation(int member_formation, int fid)
 {
-	while (member_formation >= 0) {
-		if (member_formation == fid)
+	struct formation_struct *f = get_formation(member_formation);
+	while (f) {
+		if (f->fid == fid)
 			return 1;
-		member_formation = formation[member_formation].super;
+		f = f->super;
 	}
 	return 0;
 }
 
 #define MAXPAGESIZE 1024
 struct member_form_state {
-	int pos;
+	int pos; /* state machine */
 	int fid; /* selector */
-	const struct member_struct *m;
-	int f; /* formation index */
+	const struct member_struct *m; /* member iterator */
+	const struct formation_struct *f; /* formation iterator */
 };
 
-/*
-			"<form action=\"/member\" method=\"POST\"><p>Gliederung: <input name=\"fid\" type=\"number\">"
-			"<input type=\"submit\" value=\"Abrufen\"></p></form>"
-*/
 
 /*
     pos:
@@ -145,7 +176,6 @@ struct member_form_state {
 			4 static
 			5 done
 */
-
 static ssize_t member_form_reader(void *cls, uint64_t pos, char *buf, size_t max)
 {
 	struct member_form_state *mfs = cls;
@@ -156,14 +186,13 @@ static ssize_t member_form_reader(void *cls, uint64_t pos, char *buf, size_t max
 		p = add_header(p, "Mitglieder");
 		p = stpcpy(p, "<div id=\"main\"><form action=\"/member\" method=\"POST\"><p><select name=\"fid\">");
 		mfs->pos++;
-		mfs->f = 0;
+		mfs->f = formation_list;
 		return p - buf;
 		
 	case 1:
-		if (mfs->f < nformation) {
-			const struct formation_struct *f = &formation[mfs->f];
-			p += sprintf(p, "<option %svalue=\"%d\">%s</option>", mfs->fid == mfs->f ? "selected ":"", mfs->f, f->name);
-			mfs->f++;
+		if (mfs->f) {
+			p += sprintf(p, "<option %svalue=\"%d\">%s</option>", mfs->fid == mfs->f->fid ? "selected ":"", mfs->f->fid, mfs->f->name);
+			mfs->f = mfs->f->next;
 			return p - buf;
 		}
 		mfs->pos++;
@@ -174,11 +203,11 @@ static ssize_t member_form_reader(void *cls, uint64_t pos, char *buf, size_t max
 			"<table><tr><th>Vorname</th><th>Nachname</th><th>Straße</th><th>Haus-Nr.</th>" \
 			"<th>PLZ</th><th>Ort</th><th>Gliederung</th></tr>");
 		mfs->pos++;
-		mfs->m = &member[0];
+		mfs->m = member_list;
 		return p - buf;
 	
 	case 3:
-		while (mfs->m < &member[nmember]) {
+		while (mfs->m) {
 			if (in_formation(mfs->m->formation, mfs->fid)) {
 				p = stpcpy(p, "<tr><td>");
 				p = stpcpy(p, mfs->m->first);
@@ -193,16 +222,17 @@ static ssize_t member_form_reader(void *cls, uint64_t pos, char *buf, size_t max
 				p = stpcpy(p, "</td><td>");
 				p = stpcpy(p, mfs->m->city);
 				p = stpcpy(p, "</td><td>");
-				p = stpcpy(p, formation[mfs->m->formation].name);
+				p = stpcpy(p, get_formation(mfs->m->formation)->name);
 				p = stpcpy(p, "</td></tr>");
 		
-				mfs->m++;
+				mfs->m = mfs->m->next;
 				return p - buf;
 			}
-			mfs->m++;
+			mfs->m = mfs->m->next;
 		} 
 		mfs->pos++;
 		/* no break */
+
 	case 4:
 		p = stpcpy(p, "</table></div>");
 		p = add_footer(p);
@@ -232,8 +262,16 @@ struct MHD_Response *member_form(struct Request *request)
 
 struct formation_form_state {
 	int pos;
+	const struct formation_struct *f; /* formation iterator */
 };
 
+/*
+    pos:
+			0 static
+			1 formation
+			2 static
+			3 done
+*/
 static ssize_t formation_form_reader(void *cls, uint64_t pos, char *buf, size_t max)
 {
 	struct formation_form_state *ffs = cls;
@@ -244,29 +282,33 @@ static ssize_t formation_form_reader(void *cls, uint64_t pos, char *buf, size_t 
 		p = add_header(p, "Gliederungen");
 		p = stpcpy(p, "<body><div id=\"main\"><table><tr><th>ID</th><th>Gliederung</th><th>Übergliederung</th></tr>");
 		ffs->pos++;
-		break;
+		ffs->f = formation_list;
+		return p - buf;
 
-	default:
-		if (ffs->pos < nformation + 1) {
-			const struct formation_struct *f = &formation[ffs->pos-1];
+	case 1:
+		if (ffs->f) {
       p = stpcpy(p, "<tr><td>");
-			p += sprintf(p, "%d", ffs->pos - 1);
+			p += sprintf(p, "%d", ffs->f->fid);
       p = stpcpy(p, "</td><td>");
-			p = stpcpy(p, f->name);
+			p = stpcpy(p, ffs->f->name);
       p = stpcpy(p, "</td><td>");
-			p += sprintf(p, "%d", f->super);
+			p += sprintf(p, "%d",  ffs->f->super ? ffs->f->super->fid : -1);
       p = stpcpy(p, "</td></tr>");
-			ffs->pos++;
-			break;
-			
-		} else if (ffs->pos == nformation + 1) {
+			ffs->f = ffs->f->next;
+			return p - buf;
+		} 
+		ffs->pos++;
+		/* no break */
+
+	case 2:
 			p = stpcpy(p, "</table></div>");
 			p = add_footer(p);
 			ffs->pos++;
-			break;
-		} else /* nformation + 2 */ {
-			return MHD_CONTENT_READER_END_OF_STREAM; /* no more bytes */
-		}
+			return p - buf;
+	
+	case 3:
+	default:
+		return MHD_CONTENT_READER_END_OF_STREAM; /* no more bytes */
 	}	
 
 	return p - buf;

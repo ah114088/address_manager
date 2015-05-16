fc53e61b455f37aeacace5846c60cd5d530f799d
Andreas Haas <andreas.haas1@gmx.de>
2015-05-16 19:42:02 +0200

    change password with test script

--- test/test.rb
+++ test/test.rb
@@ -2,6 +2,48 @@
 
 require 'net/http'
 
+def login http, cookies, password
+	puts "# -- login"
+	req = Net::HTTP::Post.new('/', { 'Cookie' => cookies })
+	req.set_form_data({"user" => "admin", "password" => password})
+	res = http.request(req)
+	# puts res.body
+
+	if ! res.body.include? "Willkommen"
+		puts "login failed"
+		exit 1
+	end
+end
+
+def logout http, cookies
+	puts "# -- logout"
+	req = Net::HTTP::Post.new('/logout', { 'Cookie' => cookies })
+	req.set_form_data({})
+	res = http.request(req)
+	# puts res.body
+end
+
+def passwd http, cookies, newpassword
+	puts "# -- passwd"
+	req = Net::HTTP::Post.new('/chpass', { 'Cookie' => cookies })
+	req.set_form_data({"newpassword" => newpassword})
+	res = http.request(req)
+	# puts res.body
+end
+
+def members http, cookies
+	puts "# -- query members"
+	req = Net::HTTP::Get.new('/member', { 'Cookie' => cookies })
+	req.set_form_data({})
+	res = http.request(req)
+	# puts res.body
+end
+
+if ARGV.length != 1
+	puts "usage: test.rb <URL>"
+	exit 1
+end
+
 server = URI.parse(ARGV[0])
 
 http = Net::HTTP.new(server.host, server.port)
@@ -22,21 +64,19 @@ all_cookies.each { | cookie |
 }
 cookies = cookies_array.join('; ')
 puts "Cookies: #{cookies}"
-puts res.body
+# puts res.body
 
-puts "# -- login"
-req = Net::HTTP::Post.new('/', { 'Cookie' => cookies })
-req.set_form_data({"user" => "admin", "password" => "admin"})
-res = http.request(req)
-puts res.body
+login(http, cookies, "admin")
+# members(http, cookies)
+logout(http, cookies)
 
-if ! res.body.include? "Willkommen"
-  puts "login failed"
-  exit 1
-end
+login(http, cookies, "admin")
+passwd(http, cookies, "geheim")
+logout(http, cookies)
 
-puts "# -- query members"
-req = Net::HTTP::Get.new('/member', { 'Cookie' => cookies })
-req.set_form_data({})
-res = http.request(req)
-puts res.body
+login(http, cookies, "geheim")
+logout(http, cookies)
+
+login(http, cookies, "geheim")
+passwd(http, cookies, "admin")
+logout(http, cookies)

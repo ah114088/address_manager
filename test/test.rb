8d26f01747c60f1ad6fb01a1eaad0266e138828f
Andreas Haas <andreas.haas1@gmx.de>
2015-05-17 20:21:30 +0200

    reorganized test.rb

--- test/test.rb
+++ test/test.rb
@@ -2,129 +2,120 @@
 
 require 'net/http'
 
-def login http, cookies, user, password
-	puts "# -- login"
-	req = Net::HTTP::Post.new('/', { 'Cookie' => cookies })
-	req.set_form_data({"user" => user, "password" => password})
-	res = http.request(req)
-	# puts res.body
+class Session
+	def initialize uri
+		server = URI.parse(uri)
+		@http = Net::HTTP.new(server.host, server.port)
 
-	if ! res.body.include? "Willkommen"
-		puts "login failed"
-		exit 1
+		puts "# -- request new session"
+		req = Net::HTTP::Get.new('/')
+		res = @http.request(req)
+
+		if res.code != '200'
+			puts "failure with code #{res.code}"
+			exit 1
+		end
+		all_cookies = res.get_fields('set-cookie')
+		cookies_array = Array.new
+		all_cookies.each { | cookie |
+			cookies_array.push(cookie.split('; ')[0])
+		}
+		@cookies = cookies_array.join('; ')
+		puts "Cookies: #{@cookies}"
+	end
+
+	def login user, password
+		puts "# -- login"
+		req = Net::HTTP::Post.new('/', { 'Cookie' => @cookies })
+		req.set_form_data({"user" => user, "password" => password})
+		res = @http.request(req)
+		if ! res.body.include? "Willkommen"
+			puts "login failed"
+			exit 1
+		end
+	end
+	def logout
+		puts "# -- logout"
+		req = Net::HTTP::Post.new('/logout', { 'Cookie' => @cookies })
+		res = @http.request(req)
+	end
+
+	def passwd newpassword
+		puts "# -- passwd"
+		req = Net::HTTP::Post.new('/chpass', { 'Cookie' => @cookies })
+		req.set_form_data({"newpassword" => newpassword})
+		res = @http.request(req)
+	end
+	def members
+		puts "# -- query members"
+		req = Net::HTTP::Get.new('/member', { 'Cookie' => @cookies })
+		res = @http.request(req)
+		# puts res.body
+	end
+	def newuser username, password, fid
+		puts "# -- new user"
+		req = Net::HTTP::Post.new('/newuser', { 'Cookie' => @cookies })
+		req.set_form_data({"username" => username, "password" => password, "fid" => fid})
+		res = @http.request(req)
+	end
+	def newmember first, second, street, house, zip, city, email, fid
+		puts "# -- new member"
+		req = Net::HTTP::Post.new('/newmember', { 'Cookie' => @cookies })
+		req.set_form_data({"first" => first, "second" => second, "street" => street, "house" => house, 
+				"zip" => zip, "city" => city, "email" => email, "fid" => fid})
+		res = @http.request(req)
+	end
+
+	def newformation name, fid
+		puts "# -- new formation"
+		req = Net::HTTP::Post.new('/newformation', { 'Cookie' => @cookies })
+		req.set_form_data({"name" => name, "fid" => fid})
+		res = @http.request(req)
+	end
+	def newformation_bug name, fid
+		puts "# -- new formation"
+		req = Net::HTTP::Post.new('/newformation', { 'Cookie' => @cookies })
+		req.set_form_data({"bug" => "bug", "name" => name, "fid" => fid})
+		res = @http.request(req)
 	end
 end
 
-def logout http, cookies
-	puts "# -- logout"
-	req = Net::HTTP::Post.new('/logout', { 'Cookie' => cookies })
-	req.set_form_data({})
-	res = http.request(req)
-	# puts res.body
-end
 
-def passwd http, cookies, newpassword
-	puts "# -- passwd"
-	req = Net::HTTP::Post.new('/chpass', { 'Cookie' => cookies })
-	req.set_form_data({"newpassword" => newpassword})
-	res = http.request(req)
-	# puts res.body
-end
-
-def members http, cookies
-	puts "# -- query members"
-	req = Net::HTTP::Get.new('/member', { 'Cookie' => cookies })
-	req.set_form_data({})
-	res = http.request(req)
-	# puts res.body
-end
-
-def newuser http, cookies, username, password, fid
-	puts "# -- new user"
-	req = Net::HTTP::Post.new('/newuser', { 'Cookie' => cookies })
-	req.set_form_data({"username" => username, "password" => password, "fid" => fid})
-	res = http.request(req)
-	# puts res.body
-end
-
-def newmember http, cookies, first, second, street, house, zip, city, email, fid
-	puts "# -- new member"
-	req = Net::HTTP::Post.new('/newmember', { 'Cookie' => cookies })
-	req.set_form_data({"first" => first, "second" => second, "street" => street, "house" => house, 
-			"zip" => zip, "city" => city, "email" => email, "fid" => fid})
-	res = http.request(req)
-	# puts res.body
-end
-
-def newformation http, cookies, name, fid
-	puts "# -- new formation"
-	req = Net::HTTP::Post.new('/newformation', { 'Cookie' => cookies })
-	req.set_form_data({"name" => name, "fid" => fid})
-	res = http.request(req)
-	# puts res.body
-end
-
-def newformation_bug http, cookies, name, fid
-	puts "# -- new formation"
-	req = Net::HTTP::Post.new('/newformation', { 'Cookie' => cookies })
-	req.set_form_data({"bug" => "bug", "name" => name, "fid" => fid})
-	res = http.request(req)
-	# puts res.body
-end
+# --- main ---
 
 if ARGV.length != 1
 	puts "usage: test.rb <URL>"
 	exit 1
 end
 
-server = URI.parse(ARGV[0])
+session = Session.new(ARGV[0])
 
-http = Net::HTTP.new(server.host, server.port)
+session.login "admin", "admin"
+session.members
+session.logout
 
-puts "# -- request new session"
-req = Net::HTTP::Get.new('/')
-res = http.request(req)
+session.login "admin", "admin"
+session.passwd "geheim"
+session.logout
 
-if res.code != '200'
-  puts "failure with code #{res.code}"
-  exit 1
-end
+session.login "admin", "geheim"
+session.logout
 
-all_cookies = res.get_fields('set-cookie')
-cookies_array = Array.new
-all_cookies.each { | cookie |
-  cookies_array.push(cookie.split('; ')[0])
-}
-cookies = cookies_array.join('; ')
-puts "Cookies: #{cookies}"
-# puts res.body
+session.login "admin", "geheim"
+session.passwd "admin"
+session.logout
 
-login(http, cookies, "admin", "admin")
-# members(http, cookies)
-logout(http, cookies)
+session.login "admin", "admin"
+session.newuser "mfr", "mfr", "0"
+session.logout
 
-login(http, cookies, "admin", "admin")
-passwd(http, cookies, "geheim")
-logout(http, cookies)
+session.login "mfr", "mfr"
+session.logout
 
-login(http, cookies, "admin", "geheim")
-logout(http, cookies)
-
-login(http, cookies, "admin", "geheim")
-passwd(http, cookies, "admin")
-logout(http, cookies)
-
-login(http, cookies, "admin", "admin")
-newuser(http, cookies, "mfr", "mfr", "0")
-logout(http, cookies)
-
-login(http, cookies, "mfr", "mfr")
-logout(http, cookies)
-
-login(http, cookies, "admin", "admin")
-newmember(http, cookies, "Michaela", "Muster", "Kurfürstendamm", "11", "10707", "Berlin", "mm@gmx.de", 0)
-newformation(http, cookies, "Hongkong", "0")
-logout(http, cookies)
+session.login "admin", "admin"
+session.newmember "Michaela", "Muster", "Kurfürstendamm", "11", "10707", "Berlin", "mm@gmx.de", 0
+session.newformation "Hongkong", "0"
+session.logout
 
 # these cases should be tested as well
 # newformation(http, cookies, "Hongkong", "00000000") # shoult raise an error
